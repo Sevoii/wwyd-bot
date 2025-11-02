65a878181476167124235be3ae16371e0767d9c8
a <@>
2025-11-01 23:14:33 -0400

    wwyd daily task scheduling

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,19 @@
 const { Events } = require("discord.js");
+const schedule = require("node-schedule");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+
+    const rule = new schedule.RecurrenceRule();
+    rule.hour = 10;
+    rule.minute = 0;
+    rule.tz = "Etc/UTC";
+
+    schedule.scheduleJob(rule, () => {
+      client.emit("WWYD_Daily", client);
+    });
   },
 };
---
+++ src/events/wwyd_daily.js
@@ -0,0 +1,14 @@
+const {
+  Events,
+  EmbedBuilder,
+  MessageFlags,
+  AttachmentBuilder,
+} = require("discord.js");
+
+module.exports = {
+  name: "WWYD_Daily",
+  once: false,
+  async execute(client) {
+    console.log("wwyd daily event");
+  },
+};
