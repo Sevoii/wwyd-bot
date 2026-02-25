4d6a9fdecc713d17e9cacacac89edf3d50b27fb5
a <@>
2026-02-25 16:22:52 -0500

    hotfix

--- src/db/local_sqlite3/migrations/005_streak_update_hotfix.js
+++ src/db/local_sqlite3/migrations/005_streak_update_hotfix.js
@@ -1,15 +1,16 @@
 module.exports = {
   upgrade: async (connection) => {
-    await connection.run(`UPDATE UserScore
-                          SET streak = (SELECT COUNT(*)
-                                        FROM WwydScore ws
-                                        WHERE ws.guild_id = UserScore.guild_id
-                                          AND ws.discord_id = UserScore.discord_id
-                                          AND ws.problem_id > (SELECT COALESCE(MAX(wss.problem_id), "")
-                                                               FROM WwydScore wss
-                                                               WHERE wss.correct = 0
-                                                                 AND wss.guild_id = UserScore.guild_id
-                                                                 AND wss.discord_id = UserScore.discord_id));
+    await connection.run(`
+      UPDATE UserScore
+      SET streak = (SELECT COUNT(*)
+                    FROM WwydScore ws
+                    WHERE ws.guild_id = UserScore.guild_id
+                      AND ws.discord_id = UserScore.discord_id
+                      AND ws.problem_id > (SELECT COALESCE(MAX(wss.problem_id), '')
+                                           FROM WwydScore wss
+                                           WHERE wss.correct = 0
+                                             AND wss.guild_id = UserScore.guild_id
+                                             AND wss.discord_id = UserScore.discord_id));
     `);
 
     await connection.run("UPDATE UserScore SET best_streak = streak");
