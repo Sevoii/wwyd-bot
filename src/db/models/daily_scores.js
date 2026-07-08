d0a3085de421c5e2d5398fd9472ed8c72f97c7b9
Sevoii <116461863+Sevoii@users.noreply.github.com>
2026-07-08 16:30:48 -0400

    Fix user scores from improper sql code

--- src/db/models/daily_scores.js
+++ src/db/models/daily_scores.js
@@ -83,20 +83,39 @@ module.exports = class DailyScores {
     }
   }
 
-  async getScore(guildId, discordId, season) {
+async getScore(guildId, discordId, season) {
     try {
-      return await this.db.get(
-        `SELECT UserScore.discord_id, UserScore.streak, UserScore.best_streak, SeasonScores.score, SeasonScores.attempts
-         FROM UserScore
-                LEFT JOIN SeasonScores
-                          ON UserScore.discord_id = SeasonScores.discord_id
-                            AND UserScore.guild_id = SeasonScores.guild_id
-         WHERE UserScore.guild_id = @guildId
-           AND UserScore.discord_id = @discordId
-           AND (@season = 0 OR SeasonScores.season = @season)
-         LIMIT 1;`,
-        { guildId, discordId, season },
-      );
+      if (season !== 0) {
+        return await this.db.get(
+          `SELECT UserScore.discord_id, UserScore.streak, UserScore.best_streak, SeasonScores.score, SeasonScores.attempts
+           FROM UserScore
+                  LEFT JOIN SeasonScores
+                            ON UserScore.discord_id = SeasonScores.discord_id
+                              AND UserScore.guild_id = SeasonScores.guild_id
+                              AND SeasonScores.season = @season
+           WHERE UserScore.guild_id = @guildId
+             AND UserScore.discord_id = @discordId
+           LIMIT 1;`,
+          { guildId, discordId, season },
+        );
+      } else {
+        return await this.db.get(
+          `SELECT UserScore.discord_id,
+                  UserScore.streak,
+                  UserScore.best_streak,
+                  SUM(SeasonScores.score)    AS score,
+                  SUM(SeasonScores.attempts) AS attempts
+           FROM UserScore
+                  LEFT JOIN SeasonScores
+                            ON UserScore.discord_id = SeasonScores.discord_id
+                              AND UserScore.guild_id = SeasonScores.guild_id
+           WHERE UserScore.guild_id = @guildId
+             AND UserScore.discord_id = @discordId
+           GROUP BY UserScore.discord_id, UserScore.guild_id
+           LIMIT 1;`,
+          { guildId, discordId },
+        );
+      }
     } catch (err) {
       console.error(err);
     }
